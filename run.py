c52b0031ed9ebd280a4b182c12b57c6b491420cc
Prem Nagarajan <prem.cmnagar@gmail.com>
2016-10-05 04:34:09 +0000

    r

--- run.py
+++ run.py
@@ -1,5 +1,13 @@
 import sys
 
+alpha = {}
+alpha['A'] = 10
+alpha['A'] = 11
+alpha['A'] = 12
+alpha['A'] = 13
+alpha['A'] = 14
+alpha['A'] = 15
+
 input = open('input', "r")
 for line in input:
     n = len(line)
@@ -10,7 +18,11 @@ for line in input:
             #print len
             char = line[i + 2]
             #print char
-            sys.stdout.write(char * (int(len) + 3))
+            if len in alpha.keys():
+                len = alpha(len)
+            else:
+                len = int(len)
+            sys.stdout.write(char * (len + 3))
             i = i + 2
         else:
             sys.stdout.write(line[i])
